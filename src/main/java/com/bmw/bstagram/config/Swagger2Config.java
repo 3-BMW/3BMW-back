5efdcc49a3c90d627aec589028f10f1626459004
JeongGod <jeonggu223@gmail.com>
2022-03-08 20:20:17 +0900

    [#1] fix : properties에서 code로 변경

--- src/main/java/com/bmw/bstagram/config/Swagger2Config.java
+++ src/main/java/com/bmw/bstagram/config/Swagger2Config.java
@@ -1,6 +1,5 @@
 package com.bmw.bstagram.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -15,7 +14,7 @@ public class Swagger2Config {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("v1-definition")
-                .pathsToMatch("../**")
+                .pathsToMatch("/api/**")
                 .build();
     }
     @Bean
